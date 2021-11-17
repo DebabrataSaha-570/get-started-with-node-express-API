806a0758f22a7c8797d7927a8e3b8e93617ad2c8
DebabrataSaha-570 <sahadebabrata570@gmail.com>
2021-11-17 12:57:53 +0600

    update

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const cors = require('cors')
+
 const app = express()
 const port = process.env.PORT || 5000;
 
+app.use(cors())
 
 app.get('/', (req, res) => {
     res.send('wow I am excited to learn node and express')
